cc4781a2cb21321bb1a49458dd03ced7a6278d4e
aoikusa6 <illusionisthalloween@gmail.com>
2022-08-15 14:44:12 +0700

    Fix env file

--- src/context/GitHubActions.js
+++ src/context/GitHubActions.js
@@ -1,10 +1,7 @@
-const GITHUB_URL = "https://api.github.com/"
-const GITHUB_TOKEN = "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"
-
 export const getUser = async (username) => {
-  const response = await fetch(`${GITHUB_URL}users/${username}`, {
+  const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}users/${username}`, {
     headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
+      Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
     },
   });
 
@@ -23,9 +20,9 @@ export const getRepos = async (username) => {
   const perPage = new URLSearchParams({
     per_page: 10,
   });
-  const response = await fetch(`${GITHUB_URL}users/${username}/repos?${sortedBy}&${perPage}`, {
+  const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}users/${username}/repos?${sortedBy}&${perPage}`, {
     headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
+      Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
     },
   });
 
@@ -48,10 +45,10 @@ export const searchUsers = async (currentUser, currentPage, usersPerPage) => {
     per_page: usersPerPage ? usersPerPage : 24,
   });
   const response = await fetch(
-    `${GITHUB_URL}search/users?${user}&${page}&${perPage}`,
+    `${process.env.REACT_APP_GITHUB_URL}search/users?${user}&${page}&${perPage}`,
     {
       headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
       },
     }
   );
